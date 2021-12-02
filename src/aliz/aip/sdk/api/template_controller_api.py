5eba7cba868038f738958f3021e4f391949f263b
Aliz Developers <aip-support@aliz.ai>
2021-12-02 11:36:41 +0000

    Generated new version - 0.1.0-alpha.1272

--- src/aliz/aip/sdk/api/template_controller_api.py
+++ src/aliz/aip/sdk/api/template_controller_api.py
@@ -345,6 +345,97 @@ class TemplateControllerApi(object):
             _request_timeout=params.get('_request_timeout'),
             collection_formats=collection_formats)
 
+    def delete_template(self, template_id, **kwargs):  # noqa: E501
+        """delete_template  # noqa: E501
+
+        Delete template  # noqa: E501
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass async_req=True
+        >>> thread = api.delete_template(template_id, async_req=True)
+        >>> result = thread.get()
+
+        :param async_req bool
+        :param str template_id: ID of the template (e.g. bed84f43-fefd-4461-a864-16367ea25dd9) (required)
+        :return: None
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+        kwargs['_return_http_data_only'] = True
+        if kwargs.get('async_req'):
+            return self.delete_template_with_http_info(template_id, **kwargs)  # noqa: E501
+        else:
+            (data) = self.delete_template_with_http_info(template_id, **kwargs)  # noqa: E501
+            return data
+
+    def delete_template_with_http_info(self, template_id, **kwargs):  # noqa: E501
+        """delete_template  # noqa: E501
+
+        Delete template  # noqa: E501
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass async_req=True
+        >>> thread = api.delete_template_with_http_info(template_id, async_req=True)
+        >>> result = thread.get()
+
+        :param async_req bool
+        :param str template_id: ID of the template (e.g. bed84f43-fefd-4461-a864-16367ea25dd9) (required)
+        :return: None
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+
+        all_params = ['template_id']  # noqa: E501
+        all_params.append('async_req')
+        all_params.append('_return_http_data_only')
+        all_params.append('_preload_content')
+        all_params.append('_request_timeout')
+
+        params = locals()
+        for key, val in six.iteritems(params['kwargs']):
+            if key not in all_params:
+                raise TypeError(
+                    "Got an unexpected keyword argument '%s'"
+                    " to method delete_template" % key
+                )
+            params[key] = val
+        del params['kwargs']
+        # verify the required parameter 'template_id' is set
+        if ('template_id' not in params or
+                params['template_id'] is None):
+            raise ValueError("Missing the required parameter `template_id` when calling `delete_template`")  # noqa: E501
+
+        collection_formats = {}
+
+        path_params = {}
+        if 'template_id' in params:
+            path_params['templateId'] = params['template_id']  # noqa: E501
+
+        query_params = []
+
+        header_params = {}
+
+        form_params = []
+        local_var_files = {}
+
+        body_params = None
+        # Authentication setting
+        auth_settings = ['bearer-key']  # noqa: E501
+
+        return self.api_client.call_api(
+            '/api/template/v1/templates/{templateId}', 'DELETE',
+            path_params,
+            query_params,
+            header_params,
+            body=body_params,
+            post_params=form_params,
+            files=local_var_files,
+            response_type=None,  # noqa: E501
+            auth_settings=auth_settings,
+            async_req=params.get('async_req'),
+            _return_http_data_only=params.get('_return_http_data_only'),
+            _preload_content=params.get('_preload_content', True),
+            _request_timeout=params.get('_request_timeout'),
+            collection_formats=collection_formats)
+
     def generate_code_from_template(self, body, template_id, workspace_id, **kwargs):  # noqa: E501
         """generate_code_from_template  # noqa: E501
 
