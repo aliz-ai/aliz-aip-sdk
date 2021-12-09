797bdae7f1cf1070b5cf6265faa1c6f78cd8773f
Adam Stefanidesz <adam.stefanidesz@aliz.ai>
2021-12-09 16:44:58 +0100

    Fix Aliz AIP SDK's production environment

--- src/aliz/aip/sdk/environments.py
+++ src/aliz/aip/sdk/environments.py
@@ -4,8 +4,8 @@ Environment = namedtuple("Environment", ["name", "host", "client_id", "client_se
 
 PROD = Environment("prod",
                    "https://aip-workspace-naurouosyq-ew.a.run.app",
-                   "292485802342-p290dncl40lh9sgs85d16nl06uh5f5cp.apps.googleusercontent.com",
-                   "gs://aliz-aip-dev-client-configs/aliz-aip-dev/desktop_client_secret.json")
+                   "681596136001-74tk1j4ctaejjuh81q1jbr3bl6elf76r.apps.googleusercontent.com",
+                   "gs://aliz-aip-dev-client-configs/aliz-aip-prod/desktop_client_secret.json")
 DEV = Environment("dev",
                   "https://aip-workspace-izulxl7bea-ew.a.run.app",
                   "292485802342-p290dncl40lh9sgs85d16nl06uh5f5cp.apps.googleusercontent.com",
