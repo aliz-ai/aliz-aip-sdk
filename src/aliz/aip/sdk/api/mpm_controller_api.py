b2a4a3f2e03b3f2c4871bb15f1c8554a39759245
Aliz Developers <aip-support@aliz.ai>
2021-12-14 10:14:33 +0000

    Generated new version - 0.1.0-alpha.1321

--- src/aliz/aip/sdk/api/mpm_controller_api.py
+++ src/aliz/aip/sdk/api/mpm_controller_api.py
@@ -690,6 +690,117 @@ class MpmControllerApi(object):
             _request_timeout=params.get('_request_timeout'),
             collection_formats=collection_formats)
 
+    def get_historical_plot_type_data(self, domain_id, unified_model_id, plot_type, **kwargs):  # noqa: E501
+        """get_historical_plot_type_data  # noqa: E501
+
+        Get historical plot type data  # noqa: E501
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass async_req=True
+        >>> thread = api.get_historical_plot_type_data(domain_id, unified_model_id, plot_type, async_req=True)
+        >>> result = thread.get()
+
+        :param async_req bool
+        :param str domain_id: Domain identifier (required)
+        :param str unified_model_id: ManagedService identifier and model identifier concatenated with '_' (required)
+        :param str plot_type: PlotType identifier (required)
+        :return: list[MpmHistoricalPlotDataDto]
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+        kwargs['_return_http_data_only'] = True
+        if kwargs.get('async_req'):
+            return self.get_historical_plot_type_data_with_http_info(domain_id, unified_model_id, plot_type, **kwargs)  # noqa: E501
+        else:
+            (data) = self.get_historical_plot_type_data_with_http_info(domain_id, unified_model_id, plot_type, **kwargs)  # noqa: E501
+            return data
+
+    def get_historical_plot_type_data_with_http_info(self, domain_id, unified_model_id, plot_type, **kwargs):  # noqa: E501
+        """get_historical_plot_type_data  # noqa: E501
+
+        Get historical plot type data  # noqa: E501
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass async_req=True
+        >>> thread = api.get_historical_plot_type_data_with_http_info(domain_id, unified_model_id, plot_type, async_req=True)
+        >>> result = thread.get()
+
+        :param async_req bool
+        :param str domain_id: Domain identifier (required)
+        :param str unified_model_id: ManagedService identifier and model identifier concatenated with '_' (required)
+        :param str plot_type: PlotType identifier (required)
+        :return: list[MpmHistoricalPlotDataDto]
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+
+        all_params = ['domain_id', 'unified_model_id', 'plot_type']  # noqa: E501
+        all_params.append('async_req')
+        all_params.append('_return_http_data_only')
+        all_params.append('_preload_content')
+        all_params.append('_request_timeout')
+
+        params = locals()
+        for key, val in six.iteritems(params['kwargs']):
+            if key not in all_params:
+                raise TypeError(
+                    "Got an unexpected keyword argument '%s'"
+                    " to method get_historical_plot_type_data" % key
+                )
+            params[key] = val
+        del params['kwargs']
+        # verify the required parameter 'domain_id' is set
+        if ('domain_id' not in params or
+                params['domain_id'] is None):
+            raise ValueError("Missing the required parameter `domain_id` when calling `get_historical_plot_type_data`")  # noqa: E501
+        # verify the required parameter 'unified_model_id' is set
+        if ('unified_model_id' not in params or
+                params['unified_model_id'] is None):
+            raise ValueError("Missing the required parameter `unified_model_id` when calling `get_historical_plot_type_data`")  # noqa: E501
+        # verify the required parameter 'plot_type' is set
+        if ('plot_type' not in params or
+                params['plot_type'] is None):
+            raise ValueError("Missing the required parameter `plot_type` when calling `get_historical_plot_type_data`")  # noqa: E501
+
+        collection_formats = {}
+
+        path_params = {}
+        if 'domain_id' in params:
+            path_params['domainId'] = params['domain_id']  # noqa: E501
+        if 'unified_model_id' in params:
+            path_params['unifiedModelId'] = params['unified_model_id']  # noqa: E501
+        if 'plot_type' in params:
+            path_params['plotType'] = params['plot_type']  # noqa: E501
+
+        query_params = []
+
+        header_params = {}
+
+        form_params = []
+        local_var_files = {}
+
+        body_params = None
+        # HTTP header `Accept`
+        header_params['Accept'] = self.api_client.select_header_accept(
+            ['*/*'])  # noqa: E501
+
+        # Authentication setting
+        auth_settings = ['bearer-key']  # noqa: E501
+
+        return self.api_client.call_api(
+            '/api/mpm/v1/domains/{domainId}/models/{unifiedModelId}/plotTypes/{plotType}', 'GET',
+            path_params,
+            query_params,
+            header_params,
+            body=body_params,
+            post_params=form_params,
+            files=local_var_files,
+            response_type='list[MpmHistoricalPlotDataDto]',  # noqa: E501
+            auth_settings=auth_settings,
+            async_req=params.get('async_req'),
+            _return_http_data_only=params.get('_return_http_data_only'),
+            _preload_content=params.get('_preload_content', True),
+            _request_timeout=params.get('_request_timeout'),
+            collection_formats=collection_formats)
+
     def get_mpm_enable_status(self, domain_id, **kwargs):  # noqa: E501
         """get_mpm_enable_status  # noqa: E501
 
@@ -891,7 +1002,7 @@ class MpmControllerApi(object):
     def get_plot_type_data(self, domain_id, unified_model_id, plot_type, _date, **kwargs):  # noqa: E501
         """get_plot_type_data  # noqa: E501
 
-        Get Plot type data  # noqa: E501
+        Get plot type data  # noqa: E501
         This method makes a synchronous HTTP request by default. To make an
         asynchronous HTTP request, please pass async_req=True
         >>> thread = api.get_plot_type_data(domain_id, unified_model_id, plot_type, _date, async_req=True)
@@ -916,7 +1027,7 @@ class MpmControllerApi(object):
     def get_plot_type_data_with_http_info(self, domain_id, unified_model_id, plot_type, _date, **kwargs):  # noqa: E501
         """get_plot_type_data  # noqa: E501
 
-        Get Plot type data  # noqa: E501
+        Get plot type data  # noqa: E501
         This method makes a synchronous HTTP request by default. To make an
         asynchronous HTTP request, please pass async_req=True
         >>> thread = api.get_plot_type_data_with_http_info(domain_id, unified_model_id, plot_type, _date, async_req=True)
